3689d6ad3f420bd9d0e89511087712a37a3e05fa
Toady <support@8pecxstudios.com>
2015-02-17 09:46:02 +1000

    Updated: Installer build system, Updated script version 2.2
    Here we have passed the large update to the build system and mechanics
    on how cyberfox installers are generated.
    
    This new process that is now 99% automated should reduce packaging time
    overall.
    
    How it works is the browser files are placed in there folders and the
    app.config gets updated with the required parameters
    for (architecture), (package) & (version), This change removes having to
    maintain 4 individual scripts as they are now one
    script that does all.
    
    We will add the beta installer to this new system once we decide if we
    are going to keep it full time.

--- _Build/_Build_Installers.cmd
+++ _Build/_Build_Installers.cmd
@@ -1,4 +1,4 @@
-::Copyright 2014 8pecxstudios
+::Copyright 2015 8pecxstudios
 @echo OFF
 ::----
 set BuildFolderPath=%~DP0
@@ -12,49 +12,27 @@ timeout 2 >nul
 ::---- 
 if exist "%BuildFolderPath%_CyberCTR\distribution.7z" del "%BuildFolderPath%_CyberCTR\distribution.7z"
 %zip% a -mx9 -t7z "%BuildFolderPath%_CyberCTR\distribution.7z" "%CyberCTRPath%"
-if exist "%BuildFolderPath%_CyberCTR\distribution.7z" copy /y "%BuildFolderPath%_CyberCTR\distribution.7z" "%BuildFolderPath%_Amd\_64\{ThirdParty}\"
-if exist "%BuildFolderPath%_CyberCTR\distribution.7z" copy /y "%BuildFolderPath%_CyberCTR\distribution.7z" "%BuildFolderPath%_Amd\_86\{ThirdParty}\"
-if exist "%BuildFolderPath%_CyberCTR\distribution.7z" copy /y "%BuildFolderPath%_CyberCTR\distribution.7z" "%BuildFolderPath%_Intel\_64\{ThirdParty}\"
-if exist "%BuildFolderPath%_CyberCTR\distribution.7z" copy /y "%BuildFolderPath%_CyberCTR\distribution.7z" "%BuildFolderPath%_Intel\_86\{ThirdParty}\"
+if exist "%BuildFolderPath%_CyberCTR\distribution.7z" copy /y "%BuildFolderPath%_CyberCTR\distribution.7z" "%BuildFolderPath%_Installer\{content}\ThirdParty\"
 echo. 
 ::----
 echo.Updating Changelog.rtf
 timeout 2 >nul
 ::---- 
-if exist "%BuildFolderPath%_Changelog\changelog.rtf" copy /y "%BuildFolderPath%_Changelog\changelog.rtf" "%BuildFolderPath%_Amd\_64\{Files}\"
-if exist "%BuildFolderPath%_Changelog\changelog.rtf" copy /y "%BuildFolderPath%_Changelog\changelog.rtf" "%BuildFolderPath%_Amd\_86\{Files}\"
-if exist "%BuildFolderPath%_Changelog\changelog.rtf" copy /y "%BuildFolderPath%_Changelog\changelog.rtf" "%BuildFolderPath%_Intel\_64\{Files}\"
-if exist "%BuildFolderPath%_Changelog\changelog.rtf" copy /y "%BuildFolderPath%_Changelog\changelog.rtf" "%BuildFolderPath%_Intel\_86\{Files}\"
+if exist "%BuildFolderPath%_Changelog\changelog.rtf" copy /y "%BuildFolderPath%_Changelog\changelog.rtf" "%BuildFolderPath%_Installer\{content}\setupFiles"
 timeout 2 >nul
 ::---- 
 echo.Building cyberfox installer packages!
 timeout 2 >nul
 ::---- 
-echo.Building cyberfox Intel 64 bit installer package!
-%ISSCompiler% /cc "%BuildFolderPath%_Intel\_64\Cyberfox_Intel_64.iss" 
-echo.Finished building cyberfox Intel 64 bit installer package!
-timeout 2 >nul
+echo.Building cyberfox installer package!
+%ISSCompiler% /cc "%BuildFolderPath%_Installer\generateInstaller.iss" 
+echo.Finished building cyberfox installer package!
 ::----
-echo.Building cyberfox Intel 86 bit installer package!
-%ISSCompiler% /cc "%BuildFolderPath%_Intel\_86\Cyberfox_Intel_86.iss"
-echo.Finished building cyberfox Intel 86 bit installer package!
-timeout 2 >nul
-::----
-echo.Building cyberfox AMD 64 bit installer package!
-%ISSCompiler% /cc "%BuildFolderPath%_Amd\_64\Cyberfox_AMD_64.iss"
-echo.Finished building cyberfox AMD 64 bit installer package!
-timeout 2 >nul
-::----
-echo.Building cyberfox AMD 86 bit installer package!
-%ISSCompiler% /cc "%BuildFolderPath%_Amd\_86\Cyberfox_AMD_86.iss"
-echo.Finished building cyberfox AMD 86 bit installer package!
-::----
-popd
+popd 
 echo. 
 echo. 
-echo. 
-echo.  Finished building all installer packages!
-echo.  Open _Installation folder for all outputed installation packages!
-timeout 3 >nul
+echo.Finished building the installer package!
+echo.Open _Installation folder for all outputed installation packages!
+timeout 2 >nul
 ::----
 exit /b %ERRORLEVEL%
