9b4e8e2b0bc52f13f66fbf429f57eea97ece1921
toady <support@8pecxstudios.com>
2015-03-31 17:44:38 +1000

    Added make installer and zipped archive to _build_portable.cmd.

--- _Build/_Build_Portable.cmd
+++ _Build/_Build_Portable.cmd
@@ -20,7 +20,9 @@ ECHO.Please Select Compiler Option
 ECHO.---------------------------------------         	   
 ECHO.                                                                      
 ECHO.Press 1 to Create (x86) Package.                
-ECHO.Press 2 to Create (x64) Package.               
+ECHO.Press 2 to Create (x64) Package.
+ECHO.Press 3 to Create Installer Package.
+ECHO.Press 4 to Create Zipped Package.
 ECHO.Press E to Exit.                      
 ECHO.                                                          
 ECHO.---------------------------------------         
@@ -29,12 +31,16 @@ ECHO.
 ECHO.----------- 
 ECHO.1. Create (x86) Package. 
 ECHO.2. Create (x64) Package. 
+ECHO.3  Create Installer Package.
+ECHO.4  Create Zipped Package.
 ECHO.E. E to Exit.
 ECHO.Any Key. Cancel.             
 ECHO.----------- 
-SET /P uin=Choose an option (1,2,E)? 
+SET /P uin=Choose an option (1,2,3,4,E)? 
 if /i {%uin%}=={1} (goto :Compile-Package-86)
-if /i {%uin%}=={2} (goto :Compile-Package-64) 
+if /i {%uin%}=={2} (goto :Compile-Package-64)
+if /i {%uin%}=={3} (goto :Installer-Package)
+if /i {%uin%}=={4} (goto :Zipped-Package)  
 if /i {%uin%}=={E} (goto :exit)
 if /i {%uin%}=={Exit} (goto :exit)
 if /i {%uin%}=={EXIT} (goto :exit)
@@ -218,6 +224,261 @@ goto :completed
 ::----
 ::----
 ::----
+:Installer-Package
+@echo off
+::----
+set BuildFolderPath=%~DP0
+set PortableAppsInstaller="%BuildFolderPath%_PortableApps.comInstaller\PortableApps.comInstaller.exe"
+set PortableAppsInstallerOutput="%BuildFolderPath%_Installation"
+::----
+ECHO.
+ECHO.Package Version *(Required)
+echo. Must be same as version been package
+set /P verdata=Enter Version Number (27.0.1.0)? 
+set Version=%verdata%
+:top-Installer-Package
+ECHO.
+ECHO.                                                         			   
+ECHO.Please Select Compiler Option           
+ECHO.---------------------------------------         	   
+ECHO.                                                                      
+ECHO.Press 1 to Create (x86) Portable Installer Package.                
+ECHO.Press 2 to Create (x64) Portable Installer Package.             
+ECHO.Press E to Exit.                      
+ECHO.                                                          
+ECHO.---------------------------------------         
+ECHO.
+ECHO.
+ECHO.----------- 
+ECHO.1. Create (x86) Package. 
+ECHO.2. Create (x64) Package. 
+ECHO.E. E to Exit.        
+ECHO.----------- 
+SET /P uin=Choose an option (1,2,E)? 
+if /i {%uin%}=={1} (goto :Compile-Package-86-Portable)
+if /i {%uin%}=={2} (goto :Compile-Package-64-Portable) 
+if /i {%uin%}=={E} (goto :exit)
+if /i {%uin%}=={Exit} (goto :exit)
+if /i {%uin%}=={EXIT} (goto :exit)
+if /i {%uin%}=={e} (goto :exit)
+if /i {%uin%}=={exit} (goto :exit)
+goto :top-Installer-Package
+
+:Compile-Package-86-Portable
+echo. Compilation of %Version% portable installation medium is starting!
+::----
+timeout 1 >nul
+::----
+echo. Building Amd 86 bit portable package!
+::----
+%PortableAppsInstaller% "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable"
+::----
+echo. Build Amd 86 bit package complete!
+::----
+if not exist "%BuildFolderPath%_Installation" (
+mkdir "%BuildFolderPath%_Installation"
+)
+::----
+ren "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.paf.exe"  "CyberfoxPortable_%Version%_English.Amd.86.paf.exe"
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.paf.exe" "%PortableAppsInstallerOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.paf.exe" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.paf.exe"
+)
+timeout 1 >nul
+::----
+echo. Building intel 86 bit portable package
+::----
+%PortableAppsInstaller% "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable"
+::----
+echo. Build intel 86 bit portable package complete!
+::----
+ren "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.paf.exe"  "CyberfoxPortable_%Version%_English.Intel.86.paf.exe"
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.paf.exe" "%PortableAppsInstallerOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.paf.exe" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.paf.exe"
+)
+::----
+goto :completed
+::----
+::----
+::----
+:Compile-Package-64-Portable
+echo. Compilation of %Version% portable installation medium is starting!
+::----
+timeout 1 >nul
+::----
+echo. Building Amd 64 bit portable package!
+::----
+%PortableAppsInstaller% "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable"
+::----
+echo. Build Amd 64 bit package complete!
+::----
+if not exist "%BuildFolderPath%_Installation" (
+mkdir "%BuildFolderPath%_Installation"
+)
+::----
+ren "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.paf.exe"  "CyberfoxPortable_%Version%_English.Amd.paf.exe"
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.paf.exe" "%PortableAppsInstallerOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.paf.exe" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.paf.exe"
+)
+timeout 1 >nul
+::----
+echo. Building intel 64 bit portable package
+::----
+%PortableAppsInstaller% "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable"
+::----
+echo. Build intel64 bit portable package complete!
+::----
+ren "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.paf.exe"  "CyberfoxPortable_%Version%_English.Intel.paf.exe"
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.paf.exe" "%PortableAppsInstallerOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.paf.exe" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.paf.exe"
+)
+::----
+goto :completed
+::----
+::----
+::----
+:Zipped-Package
+@echo Off
+::----
+set BuildFolderPath=%~DP0
+if not exist "%BuildFolderPath%_7ZipPortable\App\7-Zip\7za.exe" goto :bye
+set SevenZip=%BuildFolderPath%_7ZipPortable\App\7-Zip\7za.exe
+set PortableAppsZippedOutput="%BuildFolderPath%_Installation"
+::----
+ECHO.
+ECHO.Package Version *(Required)
+echo. Must be same as version been package
+set /P verdata=Enter Version Number (27.0.1.0)? 
+if /i {%verdata%}=={E} (goto :exit)
+if /i {%verdata%}=={Exit} (goto :exit)
+if /i {%verdata%}=={EXIT} (goto :exit)
+if /i {%verdata%}=={e} (goto :exit)
+if /i {%verdata%}=={exit} (goto :exit)
+set Version=%verdata%
+:top-Zipped-Package
+ECHO.
+ECHO.                                                         			   
+ECHO.Please Select Compiler Option           
+ECHO.---------------------------------------         	   
+ECHO.                                                                      
+ECHO.Press 1 to Create (x86) Zipped Installer Package.                
+ECHO.Press 2 to Create (x64) Zipped Installer Package.                
+ECHO.Press E to Exit.                      
+ECHO.                                                          
+ECHO.---------------------------------------         
+ECHO.
+ECHO.
+ECHO.----------- 
+ECHO.1. Create (x86) Package. 
+ECHO.2. Create (x64) Package.  
+ECHO.E. E to Exit.        
+ECHO.----------- 
+SET /P uin=Choose an option (1,2,E)? 
+if /i {%uin%}=={1} (goto :Compile-Package-86-Zipped)
+if /i {%uin%}=={2} (goto :Compile-Package-64-Zipped)
+if /i {%uin%}=={E} (goto :exit)
+if /i {%uin%}=={Exit} (goto :exit)
+if /i {%uin%}=={EXIT} (goto :exit)
+if /i {%uin%}=={e} (goto :exit)
+if /i {%uin%}=={exit} (goto :exit)
+goto :top-Zipped-Package
+::----
+::----
+::----
+:Compile-Package-86-Zipped
+echo. Compilation of %Version% zipped installation medium is starting!
+::----
+timeout 1 >nul
+::----
+echo. Building amd 86 bit zipped package!
+::----
+%SevenZip% a -mx9 -t7z "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.7z" "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable"
+::----
+echo. Build amd 86 bit package complete!
+::----
+if not exist "%BuildFolderPath%_Installation" (
+mkdir "%BuildFolderPath%_Installation"
+)
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.7z" "%PortableAppsZippedOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.7z" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.86.7z"
+)
+timeout 1 >nul
+::----
+echo. Building intel 86 bit zipped package
+::----
+%SevenZip% a -mx9 -t7z  "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.7z" "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable"
+::----
+echo. Build intel 86 bit zipped package complete!
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.7z" "%PortableAppsZippedOutput%"
+::----
+timeout 1 >nul
+::----
+if exist "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.7z" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.86.7z"
+)
+::----
+goto :completed
+
+:Compile-Package-64-Zipped
+echo. Compilation 0f %Version% zipped installation medium is starting!
+::----
+timeout 1 >nul
+::----
+echo. Building amd 64 bit zipped package!
+::----
+%SevenZip% a -mx9 -t7z "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.7z" "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable"
+::----
+echo. Build Amd 64 bit package complete!
+::----
+if not exist "%BuildFolderPath%_Installation" (
+mkdir "%BuildFolderPath%_Installation"
+)
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.7z" "%PortableAppsZippedOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.7z" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Amd\CyberfoxPortable_%Version%_English.Amd.7z"
+)
+timeout 1 >nul
+::----
+echo. Building intel 64 bit zipped package
+::----
+%SevenZip% a -mx9 -t7z  "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.7z" "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable"
+::----
+echo. Build intel 64 bit zipped package complete!
+::----
+copy /y "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.7z" "%PortableAppsZippedOutput%"
+::----
+timeout 1 >nul
+if exist "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.7z" (
+del /q /f "%BuildFolderPath%_CyberfoxPortable\Intel\CyberfoxPortable_%Version%_English.Intel.7z"
+)
+::----
+goto :completed
+::----
+::----
+::----
 :completed
 @echo off
 popd
@@ -226,7 +487,7 @@ echo.
 echo. 
 echo.Compilation Complete!
 ::----
-goto :exit
+goto :top
 ::----
 ::----
 ::----
