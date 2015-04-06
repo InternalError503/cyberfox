67a792e65ee01e8b7f577240bcfe2558df410b16
toady <support@8pecxstudios.com>
2015-04-06 21:55:08 +1000

    Updated: build_cyberfox.sh to version 1.7

--- _Build/_Linux/build_cyberfox.sh
+++ _Build/_Linux/build_cyberfox.sh
@@ -1,24 +1,60 @@
 # Cyberfox quick build script
-# Version: 1.6
+# Version: 1.7
 # Release channel linux
 
 #!/bin/bash
 
+# Check for git installation.
+function checkGIT(){
+	command -v git >/dev/null 2>&1 || { 
+	    echo "I require git but it's not installed would you like to install it now!."
+	    select yn in "Yes" "No"; do
+		case $yn in
+		    Yes )
+		      x-terminal-emulator -e sudo apt-get install git
+		    break;;
+		    No ) echo "I require git but it's not installed type 'sudo apt-get install git' to manually install it then run me again!." && exit;;
+		esac
+	    done   
+	 }
+}
+
+# Set linux branding identity color.
+function setIdentity(){
+	    echo "Setting branding identity to linux"
+	    sed -i "s/\(IDENTITY_BRANDING_INTEL *= *\).*/\1/" $WORKDIR/cyberfox/build/defines.sh
+	    sed -i "s/\(IDENTITY_BRANDING_LINUX *= *\).*/\11/" $WORKDIR/cyberfox/build/defines.sh
+	    echo "Setting branding identity complete!"
+}
+
+# Set chmod permissons.
+function setPerms(){
+	echo "Changing chmod of $1 to 777"
+	chmod -R 777 $1
+}
+
+# Add changing directory information to console.
+function changeDirectory(){
+
+	if [ "$1" == ".." ]; then
+		echo "Leaving $(dirname "$0") directory!"	
+		cd $1
+	else
+		echo "Entering $(dirname "$0")/$1 directory!"	
+		cd $1
+	fi	
+}
+
 # Set working directory default is ~/Documents
 WORKDIR=~/Documents
 
-command -v git >/dev/null 2>&1 || { 
-    echo "I require git but it's not installed would you like to install it now!."
-    select yn in "Yes" "No"; do
-	case $yn in
-	    Yes )
-	      xterm -e sudo apt-get install git
-	    break;;
-	    No ) echo "I require git but it's not installed type 'sudo apt-get install git' to manually install it then run me again!." && exit;;
-	esac
-    done   
- }
+# Set repository url.
+GITURI=https://github.com/InternalError503/cyberfox.git
 
+# Get and set base path.
+Dir=$(cd "$(dirname "$0")" && pwd)
+
+checkGIT
 cd $WORKDIR
 
 echo "Do you wish to setup or update cyberfox source repository now?"
@@ -26,30 +62,23 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )	  
 	  if [ -d "cyberfox" ]; then
+	      changeDirectory "cyberfox"
 	    echo "Auto purge uncommited untracked changes"
-	      cd cyberfox
 	      git checkout -- .
 	    echo "Cloning latest cyberfox source files"
 	      git pull
-	    echo "Setting branding identity to linux"
-	      sed -i "s/\(IDENTITY_BRANDING_INTEL *= *\).*/\1/" $WORKDIR/cyberfox/build/defines.sh
-	      sed -i "s/\(IDENTITY_BRANDING_LINUX *= *\).*/\11/" $WORKDIR/cyberfox/build/defines.sh 
-	      cd ..
-	    #Need to redo chmod and set permsissions every pull.
-	    echo "Changing chmod of cyberfox repository to 777"
-	      chmod -R 777 cyberfox 
-	      cd cyberfox
+			setIdentity
+	      changeDirectory ".."
+	      	setPerms "cyberfox"
+	      changeDirectory "cyberfox"
 	  else
 	    echo "Downloading cyberfox source repository"
-	      git clone https://github.com/InternalError503/cyberfox.git
-	    echo "Changing chmod of cyberfox repository to 777"
-	      chmod -R 777 cyberfox
+	    git clone $GITURI
+	      	setPerms "cyberfox"
 	    echo "mozconfig does not exist copying pre-configured to cyberfox root"
-	      cp -r $WORKDIR/cyberfox/_Build/_Linux/mozconfig $WORKDIR/cyberfox/
-	    echo "Setting branding identity to linux"
-	      sed -i "s/\(IDENTITY_BRANDING_INTEL *= *\).*/\1/" $WORKDIR/cyberfox/build/defines.sh
-	      sed -i "s/\(IDENTITY_BRANDING_LINUX *= *\).*/\11/" $WORKDIR/cyberfox/build/defines.sh
-	    cd cyberfox
+	    cp -r $WORKDIR/cyberfox/_Build/_Linux/mozconfig $WORKDIR/cyberfox/
+			setIdentity
+	      changeDirectory "cyberfox"
 	  fi; break;;
         No ) break;;
     esac
@@ -69,7 +98,7 @@ select yn in "Yes" "No"; do
 	      done
 	  fi
 	  if [ -f $WORKDIR/cyberfox/mozconfig ]; then
-	  	cd $WORKDIR/cyberfox
+	      	changeDirectory "$WORKDIR/cyberfox"
 		./mach build
 	  else
 		 echo "Were sorry but we can't build cyberfox the mozconfig is missing!" && exit
@@ -83,7 +112,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 	  if [ -d $WORKDIR/obj64/dist/bin ]; then
-	      cd cyberfox
+	      	changeDirectory "cyberfox"
 	      ./mach run
 	  else
 	      echo "Unable to start cyberfox $WORKDIR/obj64/dist/bin does not exist!"
@@ -97,7 +126,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 	  if [ -d $WORKDIR/obj64/dist/bin ]; then
-	    cd cyberfox
+	    	changeDirectory "cyberfox"
 	    ./mach package
 	  else
 	    echo "Unable to package cyberfox $WORKDIR/obj64/dist/bin does not exist!"
@@ -106,7 +135,6 @@ select yn in "Yes" "No"; do
     esac
 done
 
-Dir=$(cd "$(dirname "$0")" && pwd)
 if [ -f $Dir/bundle_cyberctr.sh ]; then
   echo "Do you wish to package CyberCTR into cyberfox now?"
   select yn in "Yes" "No"; do
