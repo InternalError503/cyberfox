225fb662a6d4f4e2a922ed01d6210293b87fba36
toady <support@8pecxstudios.com>
2015-04-17 12:16:48 +1000

    Updated: bundle_cyberctr.sh to version 1.1

--- _Build/_Linux/bundle_cyberctr.sh
+++ _Build/_Linux/bundle_cyberctr.sh
@@ -1,41 +1,78 @@
 # Get latest CyberCTR package
-# Version: 1.0
+# Version: 1.1
 
 #!/bin/bash
 
 # Set working directory default is ~/Documents
 WORKDIR=~/Documents
 
-command -v git >/dev/null 2>&1 || { 
-    echo "I require git but it's not installed would you like to install it now!."
-    select yn in "Yes" "No"; do
-	case $yn in
-	    Yes )
-	      xterm -e sudo apt-get install git
-	    break;;
-	    No ) echo "I require git but it's not installed type 'sudo apt-get install git' to manually install it then run me again!." && exit;;
-	esac
-    done   
- }
+# Set repository url.
+GITURI=https://github.com/InternalError503/CyberCTR.git
 
+# Check for git installation.
+function checkGIT(){
+  command -v git >/dev/null 2>&1 || { 
+      echo "I require git but it's not installed would you like to install it now!."
+      select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+          x-terminal-emulator -e sudo apt-get install git
+        break;;
+        No ) echo "I require git but it's not installed type 'sudo apt-get install git' to manually install it then run me again!." && exit;;
+    esac
+      done   
+   }
+}
+
+# Set on linux Simplicity Linux as default.
+function setHomeStyle(){
+      echo "Setting about:home default to (Simplicity linux)"
+      sed -i 's|\("extensions.classicthemerestorer.abouthome",\) "\(.*\)"|\1 "simplicitygreen"|' $WORKDIR/CyberCTR/distribution/bundles/CTR@8pecxstudios.com/defaults/preferences/options.js
+}
+
+# Set chmod permissons.
+function setPerms(){
+  echo "Changing chmod of $1 to 777"
+  chmod -R 777 $1
+}
+
+# Add changing directory information to console.
+function changeDirectory(){
+
+  if [ "$1" == ".." ]; then
+    echo "Leaving last directory!" 
+    cd $1
+  else
+    echo "Entering $1 directory!" 
+    cd $1
+  fi  
+}
+
+checkGIT
 cd $WORKDIR
 
-if [ -d "CyberCTR" ]; then
-  echo "Auto purge uncommited untracked changes"
-  cd CyberCTR
-  git checkout -- .
-  echo "Cloning latest CyberCTR source files"
-  git pull
-  cd ..
-  #Need to redo chmod and set permsissions every pull
-  echo "Changing chmod of CyberCTR repository to 777"
-  chmod -R 777 CyberCTR 
-else
-  echo "Downloading CyberCTR source repository"
-  git clone https://github.com/InternalError503/CyberCTR.git
-  echo "Changing chmod of CyberCTR repository to 777"
-  chmod -R 777 CyberCTR 
-fi
+echo "Do you wish to setup or update CyberCTR source repository now?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+        if [ -d "CyberCTR" ]; then
+          echo "Auto purge uncommited untracked changes"
+          changeDirectory "CyberCTR"
+          git checkout -- .
+          echo "Cloning latest CyberCTR source files"
+          git pull
+          changeDirectory ".."
+          setHomeStyle
+          setPerms "CyberCTR"
+        else
+          echo "Downloading CyberCTR source repository"
+          git clone $GITURI
+          setHomeStyle
+          setPerms "CyberCTR"
+        fi; break;;
+        No ) break;;
+    esac
+done
 
 echo "Do you wish to transfer CyberCTR into obj64/dist/cyberfox now?"
 select yn in "Yes" "No"; do
@@ -56,8 +93,7 @@ echo "Do you wish to package cyberfox archive now?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-	  cd $WORKDIR/obj64/dist
-	  
+	  changeDirectory "$WORKDIR/obj64/dist"
 	  # Get the current filename with browser version!
 	  filename=$(basename Cyberfox-*.en-US.linux-x86_64.tar.bz2)
 	  
