80fb1298a69d14e8577b29ad095bce2e1ac3e348
toady <support@8pecxstudios.com>
2015-08-28 10:53:46 +1000

    Update installer build example files.

--- _Build/_Build-Example.bat
+++ _Build/_Build-Example.bat
@@ -1,5 +1,11 @@
-set releaseVer=00.0.0.0
-set smallVer=00.0.0
+(set /P smallVer=
+)<Version\smver.txt
+(set /P releaseVer=
+)<Version\Version.txt
+call _Func_GenDirHash.bat "intel86"
+call _Func_GenDirHash.bat "intel64"
+call _Func_GenDirHash.bat "amd86"
+call _Func_GenDirHash.bat "amd64"
 BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=i368 --package=intel86 --version=%releaseVer% --sversion=%smallVer% --build
 BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=amd64 --package=intel64 --version=%releaseVer% --sversion=%smallVer% --build
 BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=i368 --package=amd86 --version=%releaseVer% --sversion=%smallVer% --build
