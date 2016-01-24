c8a2b43d73198695b6cf00bba2095cca40f1aed7
toady <support@8pecxstudios.com>
2016-01-24 14:22:25 +1000

    Added: Experimental registry entries for cyberfox beta.
    Added: Experimental registry entries for cyberfox beta.
    
    These registry entries are for testing the new cyberfox update manager,
    Beta detection an version verification in pre-alpha builds of cyberfox
    update manger are all green at this stage.
    
    Experimental in the app.config is default false which equals off.

--- _Build/_Build-Example-Beta.bat
+++ _Build/_Build-Example-Beta.bat
@@ -3,5 +3,5 @@ set smallVer=00.0
 set betaVer=b0
 call _Func_GenDirHash.bat "beta86"
 call _Func_GenDirHash.bat "beta64"
-BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=i368 --package=beta86 --version=%releaseVer% --sversion=%smallVer%%betaVer% --build
-BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=amd64 --package=beta64 --version=%releaseVer% --sversion=%smallVer%%betaVer% --build
+BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=i368 --package=beta86 --version=%releaseVer% --sversion=%smallVer% --bversion=%betaVer% --build
+BuildISS.exe --configIn=_InstallerConfig\app.config --configOut=_Installer\app.config --arch=amd64 --package=beta64 --version=%releaseVer% --sversion=%smallVer% --bversion=%betaVer% --build
