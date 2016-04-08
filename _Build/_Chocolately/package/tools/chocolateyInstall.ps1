b16320795271a12b45fcb12cd0417a431227a44f
toady <support@8pecxstudios.com>
2016-04-08 19:18:10 +1000

    Update: Chocolatey package generation files.

--- _Build/_Chocolately/package/tools/chocolateyInstall.ps1
+++ _Build/_Chocolately/package/tools/chocolateyInstall.ps1
@@ -1,9 +1,14 @@
 $packageName = 'cyberfox'
 $installerType = 'exe'
-$version = '44.0.2'
-$url = "https://download.8pecxstudios.com/latest/Cyberfox-${version}.en-US.win32.intel.exe"
-$url64 = "https://download.8pecxstudios.com/latest/Cyberfox-${version}.en-US.win64-x86_64.intel.exe"
+$version = '45.0.3'
+$url = "https://8pecxstudios.com/download/latest/Cyberfox-${version}.en-US.win32.intel.exe"
+$url64 = "https://8pecxstudios.com/download/latest/Cyberfox-${version}.en-US.win64-x86_64.intel.exe"
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
+$is64bit = (Get-ProcessorBits) -eq 64
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+if ($is64bit) {
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+} else {
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+}
