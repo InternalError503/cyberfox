55fd4cf7a234583b713be230fd45136df5244d77
toady <support@8pecxstudios.com>
2016-09-16 09:25:24 +1000

    Update: Bookmarks packaging for language packs.

--- _Build/_Language/XPI Language Copy Bookmarks.cmd
+++ _Build/_Language/XPI Language Copy Bookmarks.cmd
@@ -1,9 +1,9 @@
-@echo off
-title XPI Language Copy Bookmarks Version: 2.0.3
+@ECHO OFF
+TITLE XPI Language Copy Bookmarks Version: 2.4
 ECHO.
 ECHO.###########################################################
 ECHO.#                                                         #
-ECHO.#              Copyright(c) 2015 8pecxstudios             #
+ECHO.#              Copyright(c) 2016 8pecxstudios             #
 ECHO.#         ---------------------------------------         #
 ECHO.#                                                         #
 ECHO.#                      8pecxstudios                       #
@@ -11,8 +11,16 @@ ECHO.#                                                         #
 ECHO.#         ---------------------------------------         #
 ECHO.###########################################################
 ECHO.
-set CD=%~DP0
-if not exist "_XPI_Folder" goto :end
-set DestinPath=_XPI_Folder
-for /f %%f in ('dir /b "%DestinPath%"') do copy /y "%CD%bookmarks.html" "%DestinPath%\%%f\browser\defaults\profile"
-exit /b %ERRORLEVEL%
+SET CD=%~DP0
+IF NOT EXIST "_XPI_Folder" GOTO :END
+SET INPUTPATH=_XPI_Folder
+SET PATCHFILE=_Language-patches
+
+FOR /F %%F IN ('DIR /B "%INPUTPATH%"') DO (
+	IF EXIST "%PATCHFILE%\%%~NF.bookmarks.html" (
+		COPY /Y "%PATCHFILE%\%%~NF.bookmarks.html" "%INPUTPATH%\%%F\browser\defaults\profile"
+	) ELSE (
+		COPY /Y "%PATCHFILE%\en-US.bookmarks.html" "%INPUTPATH%\%%F\browser\defaults\profile"
+	)
+)
+EXIT /B %ERRORLEVEL%
