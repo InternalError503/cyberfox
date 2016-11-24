cde31634bad52108e4a99b98354532dbccdb3fea
toady <support@8pecxstudios.com>
2016-11-24 19:02:42 +1000

    Enable plugins.asyncdrawing by default.
    See https://8pecxstudios.com/Forums/viewtopic.php?f=5&t=1640

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2787,13 +2787,9 @@ pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 // Asynchronous plugin initialization is on hold.
 pref("dom.ipc.plugins.asyncInit.enabled", false);
 
-#ifdef RELEASE_BUILD
 // Allow the AsyncDrawing mode to be used for plugins.
-pref("dom.ipc.plugins.asyncdrawing.enabled", false);
-#else
-// Allow the AsyncDrawing mode to be used for plugins.
+// Enable for now by default due to flash games.
 pref("dom.ipc.plugins.asyncdrawing.enabled", true);
-#endif
 
 pref("dom.ipc.processCount", 1);
 
