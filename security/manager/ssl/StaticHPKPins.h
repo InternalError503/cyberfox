cb9a026b15527d09888ce60c54646501bd756d9b
toady <support@8pecxstudios.com>
2018-03-17 09:20:12 +1000

    Merge Cyberfox 52.7.1 into master

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1184,4 +1184,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1528923209940000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1529611134914000);
